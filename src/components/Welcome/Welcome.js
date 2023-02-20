913dd75c835f281d803e24534c4c4a52d632a526
Kacper Wójcicki <kacper.wojcicki@protonmail.com>
2023-02-21 00:35:25 +0100

    Fix background cover

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -2,7 +2,7 @@ import "./Welcome.css";
 import React from "react";
 
 function Welcome() {
-    return <div className="typediv">
+    return <div className="container typediv">
         <h1 className="typewrite" data-type='[ "Hello, welcome to my site!","I am a Java Developer.", "I am happy you are here ;)" ]'></h1>
     </div>
 }
