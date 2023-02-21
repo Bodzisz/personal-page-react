eda7cc3c2540907c165a8e3d67f53efaf6d6d662
Kacper Wójcicki <kacper.wojcicki@protonmail.com>
2023-02-21 19:13:43 +0100

    Add arrow to main page

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,8 +1,17 @@
 import Welcome from "../Welcome/Welcome";
 import React from "react";
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+import "./MainPage.css";
 
 function MainPage() {
-  return <Welcome />;
+  return (
+    <div className="container main-page-div">
+      <Welcome />
+      <a href="#">
+        <MdOutlineKeyboardArrowDown className="down-arrow grow" size="100" />
+      </a>
+    </div>
+  );
 }
 
 export default MainPage;
