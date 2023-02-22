bbe2f3c7787f75802e32df63f75c405b4e85d573
Kacper Wójcicki <kacper.wojcicki@protonmail.com>
2023-02-22 21:25:13 +0100

    Updates

---
+++ precache-manifest.f4e88e42f0ac29200d7d127d3d545538.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf7b89b6ddbcf04933dc",
+    "url": "/personal-page-react/static/css/main.16abea87.chunk.css"
+  },
+  {
+    "revision": "bf7b89b6ddbcf04933dc",
+    "url": "/personal-page-react/static/js/main.bf7b89b6.chunk.js"
+  },
+  {
+    "revision": "62cac4abb9d6098befd4",
+    "url": "/personal-page-react/static/js/1.62cac4ab.chunk.js"
+  },
+  {
+    "revision": "ee65ab82a9afe99a4dbd",
+    "url": "/personal-page-react/static/css/2.c9e2c763.chunk.css"
+  },
+  {
+    "revision": "ee65ab82a9afe99a4dbd",
+    "url": "/personal-page-react/static/js/2.ee65ab82.chunk.js"
+  },
+  {
+    "revision": "6dd475513b03d57c44f2",
+    "url": "/personal-page-react/static/js/runtime~main.6dd47551.js"
+  },
+  {
+    "revision": "128db2fbf089d6609adfbffebf058e98",
+    "url": "/personal-page-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6e60798b3ba0a992b0a6b23378797f32.js"
+  "/personal-page-react/precache-manifest.f4e88e42f0ac29200d7d127d3d545538.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/personal-page-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
