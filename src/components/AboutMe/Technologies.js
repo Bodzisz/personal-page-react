c8332551c9f1c620c94384204a81e55c4255ba92
Kacper Wójcicki <kacper.wojcicki@protonmail.com>
2023-02-25 16:14:38 +0100

    Fix click on project icon for mobiles

--- src/components/AboutMe/Technologies.js
+++ src/components/AboutMe/Technologies.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   SiSpring,
   SiHibernate,
@@ -24,88 +24,116 @@ function TechnologiesIcons() {
     git: false,
   };
   const [isShown, setIsShown] = useState(isShownInitialData);
+  const [usingTouch, setUsingTouch] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("touchstart", handleTouchStart());
+
+    return () => {
+      window.removeEventListener("touchstart", handleTouchStart());
+    };
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("touchend", handleTouchEnd());
+
+    return () => {
+      window.removeEventListener("touchend", handleTouchEnd());
+    };
+  }, []);
+
+  const handleTouchStart = () => {
+    setUsingTouch(true);
+  };
+  const handleTouchEnd = () => {
+    setUsingTouch(false);
+  };
+
+  const setIconShown = (fieldName, value, shouldUseTouch) => {
+    if (usingTouch === shouldUseTouch) {
+      setIsShown({ ...isShown, [fieldName]: value });
+    }
+  };
 
   return (
     <div className="technologies-grid">
       <div
         className="icon-div"
-        onMouseEnter={() => setIsShown({ ...isShown, spring: true })}
-        onMouseLeave={() => setIsShown({ ...isShown, spring: false })}
-        onClick={() => setIsShown({ ...isShown, spring: !isShown.spring })}
+        onMouseEnter={() => setIconShown("spring", true, false)}
+        onMouseLeave={() => setIconShown("spring", false, false)}
+        onClick={() => setIconShown("spring", !isShown.spring, true)}
       >
         <SiSpring className="grow" size="100" color="#1a751d" />
         {isShown.spring ? <p className="icon-desc">Spring</p> : null}
       </div>
       <div
         className="icon-div"
-        onMouseEnter={() => setIsShown({ ...isShown, hibernate: true })}
-        onMouseLeave={() => setIsShown({ ...isShown, hibernate: false })}
-        onClick={() =>
-          setIsShown({ ...isShown, hibernate: !isShown.hibernate })
-        }
+        onMouseEnter={() => setIconShown("hibernate", true, false)}
+        onMouseLeave={() => setIconShown("hibernate", false, false)}
+        onClick={() => setIconShown("hibernate", !isShown.hibernate, true)}
       >
         <SiHibernate className="grow" size="100" color="#e8b15f" />
         {isShown.hibernate ? <p className="icon-desc">Hibernate</p> : null}
       </div>
       <div
         className="icon-div"
-        onMouseEnter={() => setIsShown({ ...isShown, sql: true })}
-        onMouseLeave={() => setIsShown({ ...isShown, sql: false })}
-        onClick={() => setIsShown({ ...isShown, sql: !isShown.sql })}
+        onMouseEnter={() => setIconShown("sql", true, false)}
+        onMouseLeave={() => setIconShown("sql", false, false)}
+        onClick={() => setIconShown("sql", !isShown.sql, true)}
       >
         <AiOutlineConsoleSql className="grow" size="100" color="#d4cfc9" />
         {isShown.sql ? <p className="icon-desc">SQL</p> : null}
       </div>
       <div
         className="icon-div"
-        onMouseEnter={() => setIsShown({ ...isShown, react: true })}
-        onMouseLeave={() => setIsShown({ ...isShown, react: false })}
-        onClick={() => setIsShown({ ...isShown, react: !isShown.react })}
+        onMouseEnter={() => setIconShown("react", true, false)}
+        onMouseLeave={() => setIconShown("react", false, false)}
+        onClick={() => setIconShown("react", !isShown.react, true)}
       >
         <SiReact className="grow" size="100" color="#2e92f0" />
         {isShown.react ? <p className="icon-desc">React</p> : null}
       </div>
       <div
         className="icon-div"
-        onMouseEnter={() => setIsShown({ ...isShown, linux: true })}
-        onMouseLeave={() => setIsShown({ ...isShown, linux: false })}
-        onClick={() => setIsShown({ ...isShown, linux: !isShown.linux })}
+        onMouseEnter={() => setIconShown("linux", true, false)}
+        onMouseLeave={() => setIconShown("linux", false, false)}
+        onClick={() => setIconShown("linux", !isShown.linux, true)}
       >
         <FcLinux className="grow" size="100" />
         {isShown.linux ? <p className="icon-desc">Linux</p> : null}
       </div>
       <div
         className="icon-div"
-        onMouseEnter={() => setIsShown({ ...isShown, docker: true })}
-        onMouseLeave={() => setIsShown({ ...isShown, docker: false })}
-        onClick={() => setIsShown({ ...isShown, docker: !isShown.docker })}
+        onMouseEnter={() => setIconShown("docker", true, false)}
+        onMouseLeave={() => setIconShown("docker", false, false)}
+        onClick={() => setIconShown("docker", !isShown.docker, true)}
       >
         <SiDocker className="grow" size="100" color="#0e6bc2" />
         {isShown.docker ? <p className="icon-desc">Docker</p> : null}
       </div>
       <div
         className="icon-div"
-        onMouseEnter={() => setIsShown({ ...isShown, python: true })}
-        onMouseLeave={() => setIsShown({ ...isShown, python: false })}
-        onClick={() => setIsShown({ ...isShown, python: !isShown.python })}
+        onMouseEnter={() => setIconShown("python", true, false)}
+        onMouseLeave={() => setIconShown("python", false, false)}
+        onClick={() => setIconShown("python", !isShown.python, true)}
       >
         <SiPython className="grow" size="100" color="#a39b03" />
         {isShown.python ? <p className="icon-desc">Python</p> : null}
       </div>
       <div
         className="icon-div"
-        onMouseEnter={() => setIsShown({ ...isShown, html: true })}
-        onMouseLeave={() => setIsShown({ ...isShown, html: false })}
-        onClick={() => setIsShown({ ...isShown, html: !isShown.html })}
+        onMouseEnter={() => setIconShown("html", true, false)}
+        onMouseLeave={() => setIconShown("html", false, false)}
+        onClick={() => setIconShown("html", !isShown.html, true)}
       >
         <SiHtml5 className="grow" size="100" color="#eb9307" />
         {isShown.html ? <p className="icon-desc">HTML5</p> : null}
       </div>
       <div
         className="icon-div"
-        onMouseEnter={() => setIsShown({ ...isShown, git: true })}
-        onMouseLeave={() => setIsShown({ ...isShown, git: false })}
-        onClick={() => setIsShown({ ...isShown, git: !isShown.git })}
+        onMouseEnter={() => setIconShown("git", true, false)}
+        onMouseLeave={() => setIconShown("git", false, false)}
+        onClick={() => setIconShown("git", !isShown.git, true)}
       >
         <SiGit className="grow" size="100" color="#e34e34" />
         {isShown.git ? <p className="icon-desc">GIT</p> : null}
