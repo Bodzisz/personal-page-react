2ec1667b2876282081ac80bdefcdae64e9c3563b
Kacper Wójcicki <kacper.wojcicki@protonmail.com>
2024-01-24 21:14:13 +0100

    Add mantine component library to project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "@mantine/core/styles.css";
 import App from "./App";
+import { MantineProvider, createTheme } from "@mantine/core";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const theme = createTheme({
+  /** To be filled */
+});
+
 root.render(
   <React.StrictMode>
-    <App />
+    <MantineProvider theme={theme}>
+      <App />
+    </MantineProvider>
   </React.StrictMode>
 );
