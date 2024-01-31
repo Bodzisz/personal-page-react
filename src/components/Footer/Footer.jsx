3bbb1a3afb58d5ea8ac1eb49d5abe2c7b27643fc
Kacper Wójcicki <kacper.wojcicki@protonmail.com>
2024-01-31 18:16:07 +0100

    Refactor footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import classes from "./Footer.module.css";
 import { BsGithub, BsTwitter, BsLinkedin } from "react-icons/bs";
-import { Anchor } from "@mantine/core";
+import { Anchor, Group, Container } from "@mantine/core";
 
 function Footer() {
   return (
     <footer className={classes.footer}>
-      <ul className={classes.footerContent}>
-        <li className={classes.social}>
+      <Group display={"flex"} justify="center">
+        <Container className={classes.social} pt={20}>
           <Anchor href="https://twitter.com/k4cper_wojcicki" underline="never" className={classes.hover}>
             <BsTwitter />
           </Anchor>
-        </li>
-        <li className={classes.social}>
+        </Container>
+        <Container className={classes.social} pt={20}>
           <Anchor href="https://github.com/Bodzisz" underline="never" className={classes.hover}>
             <BsGithub />
           </Anchor>
-        </li>
-        <li className={classes.social}>
+        </Container>
+        <Container className={classes.social} pt={20}>
           <Anchor
             href="https://pl.linkedin.com/in/kacper-wojcicki-05a13521b"
             underline="never"
@@ -25,8 +25,8 @@ function Footer() {
           >
             <BsLinkedin />
           </Anchor>
-        </li>
-      </ul>
+        </Container>
+      </Group>
     </footer>
   );
 }
