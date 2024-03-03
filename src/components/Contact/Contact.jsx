b6de82813d60bd92197c4acf674665c5856b9719
Kacper Wójcicki <kacper.wojcicki@protonmail.com>
2024-03-03 18:30:51 +0100

    Fix contact form sizing

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import "./Contact.css";
 import Typewriter from "typewriter-effect";
 import { Button, Center, Container, Input, Textarea, Title } from "@mantine/core";
 
 function Contact() {
   return (
-    <Container className="form-div" pt={150}>
+    <Container pt={150} h={"calc(100vh - 90px)"}>
       <Center>
         <Title order={3} pb={10}>
           <Typewriter
@@ -17,30 +16,32 @@ function Contact() {
           />
         </Title>
       </Center>
-      <form action="https://formsubmit.co/kacper.wojcicki@protonmail.com" method="POST">
-        <input type="hidden" name="_subject" value="Personal Page Email" />
+      <Container>
+        <form action="https://formsubmit.co/kacper.wojcicki@protonmail.com" method="POST">
+          <input type="hidden" name="_subject" value="Personal Page Email" />
 
-        <Textarea
-          type="text"
-          name="message"
-          rows="5"
-          placeholder="Type your message"
-          autosize
-          minRows={10}
-          maxRows={20}
-          withAsterisk
-          required
-          pb={10}
-        />
+          <Textarea
+            type="text"
+            name="message"
+            rows="5"
+            placeholder="Type your message"
+            autosize
+            minRows={15}
+            maxRows={25}
+            withAsterisk
+            required
+            pb={10}
+          />
 
-        <Input type="email" name="email" placeholder="Your email address" required pb={10} />
+          <Input type="email" name="email" placeholder="Your email address" required pb={10} />
 
-        <Center>
-          <Button type="submit" className="submit-button" w={"100%"}>
-            Send
-          </Button>
-        </Center>
-      </form>
+          <Center>
+            <Button type="submit" className="submit-button" w={"100%"}>
+              Send
+            </Button>
+          </Center>
+        </form>
+      </Container>
     </Container>
   );
 }
