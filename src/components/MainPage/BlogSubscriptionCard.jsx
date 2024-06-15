4538a65075171c03f94123744ec4185b8e16ec1f
Kacper Wójcicki <kacper.wojcicki@protonmail.com>
2024-06-15 18:27:39 +0200

    Adjust landing page blog subscription card

--- src/components/MainPage/BlogSubscriptionCard.jsx
+++ src/components/MainPage/BlogSubscriptionCard.jsx
@@ -1,13 +1,26 @@
-import { Card, Text, Button, Center, Anchor, Group } from "@mantine/core";
+import { Card, Text, Button, Center, Anchor, Group, useMantineTheme } from "@mantine/core";
 import { BsMedium } from "react-icons/bs";
+import { useMediaQuery } from "@mantine/hooks";
 
 const BlogSubscriptionCard = () => {
+  const theme = useMantineTheme();
+  const isMobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
+
   return (
-    <Card w={"50%"} shadow="sm" padding="lg" radius="md" withBorder style={{ background: "rgba(0, 0, 0, 0.3)" }}>
+    <Card
+      w={isMobile ? "90%" : "50%"}
+      shadow="sm"
+      padding="lg"
+      radius="md"
+      withBorder
+      style={{ background: "rgba(0, 0, 0, 0.3)" }}
+    >
+      <Center>
+        <BsMedium size={30} />
+      </Center>
       <Center>
         <Group>
           <h2 fw={500}>Get an email whenever I post</h2>
-          <BsMedium size={30} />
         </Group>
       </Center>
 
